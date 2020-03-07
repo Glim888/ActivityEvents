cdc2988c8c2cd25658d728ee5894ce066d9ae063
Lukas Oertel <annoluk@googlemail.com>
2020-03-07 14:26:50 +0100

    initial commit
    Signed-off-by: Lukas Oertel <annoluk@googlemail.com>

---
+++ ActivityEvents/ActivityEvents.yy
@@ -0,0 +1,86 @@
+{
+    "id": "8764257a-190e-4f72-a06d-d540e3dc34d9",
+    "modelName": "GMExtension",
+    "mvc": "1.2",
+    "name": "ActivityEvents",
+    "IncludedResources": [
+        
+    ],
+    "androidPermissions": [
+        
+    ],
+    "androidProps": true,
+    "androidactivityinject": "",
+    "androidclassname": "ActivityEvents",
+    "androidinject": "",
+    "androidmanifestinject": "",
+    "androidsourcedir": "",
+    "author": "",
+    "classname": "",
+    "copyToTargets": -1,
+    "date": "2019-24-25 03:12:27",
+    "description": "",
+    "exportToGame": true,
+    "extensionName": "",
+    "files": [
+        {
+            "id": "7f16bf30-76bf-40dd-8fad-a4c0dec51619",
+            "modelName": "GMExtensionFile",
+            "mvc": "1.0",
+            "ProxyFiles": [
+                
+            ],
+            "constants": [
+                
+            ],
+            "copyToTargets": -1,
+            "filename": "ActivityEvents.ext",
+            "final": "",
+            "functions": [
+                
+            ],
+            "init": "",
+            "kind": 4,
+            "order": [
+                
+            ],
+            "origname": "",
+            "uncompress": false
+        }
+    ],
+    "gradleinject": "",
+    "helpfile": "",
+    "installdir": "",
+    "iosProps": false,
+    "iosSystemFrameworkEntries": [
+        
+    ],
+    "iosThirdPartyFrameworkEntries": [
+        
+    ],
+    "iosdelegatename": "",
+    "iosplistinject": "",
+    "license": "",
+    "maccompilerflags": "",
+    "maclinkerflags": "",
+    "macsourcedir": "",
+    "options": null,
+    "optionsFile": "options.json",
+    "packageID": "",
+    "productID": "",
+    "sourcedir": "",
+    "supportedTargets": -1,
+    "tvosProps": false,
+    "tvosSystemFrameworkEntries": [
+        
+    ],
+    "tvosThirdPartyFrameworkEntries": [
+        
+    ],
+    "tvosclassname": "",
+    "tvosdelegatename": "",
+    "tvosmaccompilerflags": "",
+    "tvosmaclinkerflags": "",
+    "tvosplistinject": "",
+    "version": "0.0.1"
+}
---
+++ ActivityEvents/AndroidSource/Java/ActivityEvents.java
@@ -0,0 +1,124 @@
+package ${YYAndroidPackageName};
+
+//Game Maker Studio 2 Packages
+import ${YYAndroidPackageName}.R;
+import com.yoyogames.runner.RunnerJNILib;
+import ${YYAndroidPackageName}.RunnerActivity;
+
+//Some Android Packages
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Dialog;
+import android.view.MotionEvent;
+
+//Starts Here
+public class ActivityEvents implements IExtensionBase {
+
+    /** Called when the activity restarted */
+
+    public void onRestart() {
+        int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+        RunnerJNILib.DsMapAddString(dsMapIndex, "activity", "onRestart");
+        RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, 70);
+    }
+
+    /** Called when the activity is about to become visible. */
+
+    public void onStart() {
+
+        int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+        RunnerJNILib.DsMapAddString(dsMapIndex, "activity", "onStart");
+        RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, 70);
+    }
+
+    /** Called when the activity has become visible. */
+
+    public void onResume() {
+
+        int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+        RunnerJNILib.DsMapAddString(dsMapIndex, "activity", "onResume");
+        RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, 70);
+    }
+
+    /** Called when another activity is taking focus. */
+
+    public void onPause() {
+        int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+        RunnerJNILib.DsMapAddString(dsMapIndex, "activity", "onPause");
+        RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, 70);
+    }
+
+    /** Called when the activity is no longer visible. */
+
+    public void onStop() {
+        int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+        RunnerJNILib.DsMapAddString(dsMapIndex, "activity", "onStop");
+        RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, 70);
+    }
+
+    /** Called just before the activity is destroyed. */
+
+    public void onDestroy() {
+       // useless to create ds map, because it will never reach the social event!
+    }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    }
+
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return false;
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    public Dialog onCreateDialog(int id) {
+        return null;
+    }
+
+    public boolean onTouchEvent(final MotionEvent event) {
+        return false;
+    }
+
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        return false;
+    }
+
+    public boolean dispatchGenericMotionEvent(MotionEvent event) {
+        return false;
+    }
+
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        return false;
+    }
+
+
+}
